950bfba71f35d965e9f3f5a89a020e31f0a9780c
Hickma <hickmamoh@yahoo.com>
2023-07-04 10:04:58 +0000

    added js file

---
+++ index.js
@@ -0,0 +1,11 @@
+function changeCity() {
+  let city = prompt("What city do you live in?");
+  let temperature = prompt("What temperature is it?");
+  if (temperature <= 8) {
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = "🥶 " + " <br> Currently " + temperature + "°C in " + city;
+  } else {
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = "🥵 " + " <br> Currently " + temperature + "°C in " + city;
+  }
+}
